c206821a493d84726aef24be44af52290e280023
Ojus Rane [el16or] <el16or@eentpc128.leeds.ac.uk>
2017-11-24 12:00:48 +0000

    Version 1.02
    - In this version I have managed to insert two more sets of stopwatch and got all the working at the same time
    - In the next version, I aim to make a start with the timer, which may take me a while to get working properly due to the complexity of the picker view and having to get it to communicate with the text label
    - I will need to use UIScrollview to fit all my elements inside the screen properly
    - I have added a button called "Reset all stopwatches", which allows you to reset all 3 stopwatches at the same time rather than have to reset it one by one. This was not originally in my plans, hence you may not see this in my documentation

--- MultipleStopwatchTimer/StopwatchViewController.h
+++ MultipleStopwatchTimer/StopwatchViewController.h
@@ -9,15 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface StopwatchViewController : UIViewController
-
-//Stopwatch 1
-
 {
     NSTimer *StopwatchOne;
-    int CountStopwatchone;
+    int CountStopwatchOne;
     BOOL RunningStopwatchOne;
+    
+    NSTimer *StopwatchTwo;
+    int CountStopwatchTwo;
+    BOOL RunningStopwatchTwo;
+    
+    NSTimer *StopwatchThree;
+    int CountStopwatchThree;
+    BOOL RunningStopwatchThree;
 }
 
+//Stopwatch 1
 @property (weak, nonatomic) IBOutlet UILabel *StopwatchLabelOne;
 @property (weak, nonatomic) IBOutlet UIButton *StartButtonOne;
 @property (weak, nonatomic) IBOutlet UIButton *ResetButtonOne;
@@ -26,6 +32,41 @@
 
 - (void) updateStopwatchOne;
 - (void) STOPStopwatchOne;
+
+//Stopwatch 2
+@property (weak, nonatomic) IBOutlet UILabel *StopwatchLabelTwo;
+@property (weak, nonatomic) IBOutlet UIButton *StartButtonTwo;
+@property (weak, nonatomic) IBOutlet UIButton *ResetButtonTwo;
+- (IBAction)StartButtonTwoPressed:(UIButton *)sender;
+- (IBAction)ResetButtonTwoPressed:(UIButton *)sender;
+
+- (void) updateStopwatchTwo;
+- (void) STOPStopwatchTwo;
+
+//Stopwatch 3
+@property (weak, nonatomic) IBOutlet UILabel *StopwatchLabelThree;
+@property (weak, nonatomic) IBOutlet UIButton *StartButtonThree;
+@property (weak, nonatomic) IBOutlet UIButton *ResetButtonThree;
+- (IBAction)StartButtonThreePressed:(UIButton *)sender;
+- (IBAction)ResetButtonThreePressed:(UIButton *)sender;
+
+- (void) updateStopwatchThree;
+- (void) STOPStopwatchThree;
+
+
+//Reset all stopwatches
+- (IBAction)ResetAllStopwatchesButtonPressed:(UIButton *)sender;
+
+- (void) ResetAllStopwatches;
+
+
+
+
+
+
+
+
+
 
 
 
--- MultipleStopwatchTimer/StopwatchViewController.m
+++ MultipleStopwatchTimer/StopwatchViewController.m
@@ -20,7 +20,11 @@
     
     self.StopwatchLabelOne.text = @"00:00:00:00";
     RunningStopwatchOne = NO;
-    CountStopwatchone = 0;
+    CountStopwatchOne = 0;
+    
+    self.StopwatchLabelTwo.text = @"00:00:00:00";
+    RunningStopwatchTwo = NO;
+    CountStopwatchTwo = 0;
     
 }
 
@@ -30,6 +34,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+//Stopwatch 1
 
 - (IBAction)StartButtonOnePressed:(UIButton *)sender {
     if (RunningStopwatchOne == NO) {
@@ -43,23 +48,23 @@
         else {
             [self STOPStopwatchOne];
         }
-    }
+}
 
 
 - (IBAction)ResetButtonOnePressed:(UIButton *)sender {
     [self STOPStopwatchOne];
-    CountStopwatchone = 0;
+    CountStopwatchOne = 0;
     self.StopwatchLabelOne.text = @"00:00:00:00";
     
 }
 
 - (void) updateStopwatchOne{
-    CountStopwatchone ++;
+    CountStopwatchOne ++;
     
-    int Hours_StopwatchOne = floor(CountStopwatchone/100/3600);
-    int Minutes_StopwatchOne = floor(CountStopwatchone/100/60);
-    int Seconds_StopwatchOne = floor(CountStopwatchone/100);
-    int Milliseconds_StopwatchOne = CountStopwatchone % 100;
+    int Hours_StopwatchOne = floor(CountStopwatchOne/100/3600);
+    int Minutes_StopwatchOne = floor(CountStopwatchOne/100/60);
+    int Seconds_StopwatchOne = floor(CountStopwatchOne/100);
+    int Milliseconds_StopwatchOne = CountStopwatchOne % 100;
     
     if (Seconds_StopwatchOne >=60){
         Seconds_StopwatchOne = Seconds_StopwatchOne % 60;
@@ -75,4 +80,119 @@
     [_StartButtonOne setTitle:@"Start" forState:UIControlStateNormal];
 }
 
+
+
+//Stopwatch 2
+
+- (IBAction)StartButtonTwoPressed:(UIButton *)sender {
+    if (RunningStopwatchTwo == NO) {
+        RunningStopwatchTwo = YES;
+        [_StartButtonTwo setTitle:@"Stop" forState:UIControlStateNormal];
+        
+        if (StopwatchTwo == nil){
+            StopwatchTwo = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(updateStopwatchTwo) userInfo:nil repeats:YES];
+        }
+    }
+    else {
+        [self STOPStopwatchTwo];
+    }
+}
+
+
+- (IBAction)ResetButtonTwoPressed:(UIButton *)sender {
+    [self STOPStopwatchTwo];
+    CountStopwatchTwo = 0;
+    self.StopwatchLabelTwo.text = @"00:00:00:00";
+}
+
+
+- (void) updateStopwatchTwo{
+    CountStopwatchTwo ++;
+    
+    int Hours_StopwatchTwo = floor(CountStopwatchTwo/100/3600);
+    int Minutes_StopwatchTwo = floor(CountStopwatchTwo/100/60);
+    int Seconds_StopwatchTwo = floor(CountStopwatchTwo/100);
+    int Milliseconds_StopwatchTwo = CountStopwatchTwo % 100;
+    
+    if (Seconds_StopwatchTwo >=60){
+        Seconds_StopwatchTwo = Seconds_StopwatchTwo % 60;
+    }
+    
+    self.StopwatchLabelTwo.text = [NSString stringWithFormat:@"%02d:%02d:%02d:%02d", Hours_StopwatchTwo,Minutes_StopwatchTwo,Seconds_StopwatchTwo,Milliseconds_StopwatchTwo];
+    
+}
+- (void) STOPStopwatchTwo{
+    RunningStopwatchTwo = NO;
+    [StopwatchTwo invalidate];
+    StopwatchTwo = nil;
+    [_StartButtonTwo setTitle:@"Start" forState:UIControlStateNormal];
+}
+
+
+//Stopwatch 3
+
+- (IBAction)StartButtonThreePressed:(UIButton *)sender {
+    if (RunningStopwatchThree == NO) {
+        RunningStopwatchThree = YES;
+        [_StartButtonThree setTitle:@"Stop" forState:UIControlStateNormal];
+        
+        if (StopwatchThree == nil){
+            StopwatchThree = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(updateStopwatchThree) userInfo:nil repeats:YES];
+        }
+    }
+    else {
+        [self STOPStopwatchThree];
+    }
+}
+
+
+- (IBAction)ResetButtonThreePressed:(UIButton *)sender {
+    [self STOPStopwatchThree];
+    CountStopwatchThree = 0;
+    self.StopwatchLabelThree.text = @"00:00:00:00";
+}
+
+- (void) updateStopwatchThree{
+    CountStopwatchThree ++;
+    
+    int Hours_StopwatchThree = floor(CountStopwatchThree/100/3600);
+    int Minutes_StopwatchThree = floor(CountStopwatchThree/100/60);
+    int Seconds_StopwatchThree = floor(CountStopwatchThree/100);
+    int Milliseconds_StopwatchThree = CountStopwatchThree % 100;
+    
+    if (Seconds_StopwatchThree >=60){
+        Seconds_StopwatchThree = Seconds_StopwatchThree % 60;
+    }
+    
+    self.StopwatchLabelThree.text = [NSString stringWithFormat:@"%02d:%02d:%02d:%02d", Hours_StopwatchThree,Minutes_StopwatchThree,Seconds_StopwatchThree,Milliseconds_StopwatchThree];
+    
+}
+- (void) STOPStopwatchThree{
+    RunningStopwatchThree = NO;
+    [StopwatchThree invalidate];
+    StopwatchThree = nil;
+    [_StartButtonThree setTitle:@"Start" forState:UIControlStateNormal];
+}
+
+
+
+//Reset Button to reset all Stopwatches
+- (IBAction)ResetAllStopwatchesButtonPressed:(UIButton *)sender {
+    [self ResetAllStopwatches];
+}
+
+-(void) ResetAllStopwatches{
+    [self STOPStopwatchOne];
+    CountStopwatchOne = 0;
+    self.StopwatchLabelOne.text = @"00:00:00:00";
+    
+    [self STOPStopwatchTwo];
+    CountStopwatchTwo = 0;
+    self.StopwatchLabelTwo.text = @"00:00:00:00";
+    
+    [self STOPStopwatchThree];
+    CountStopwatchThree = 0;
+    self.StopwatchLabelThree.text = @"00:00:00:00";
+    
+}
 @end
