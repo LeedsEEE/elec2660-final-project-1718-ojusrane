7e1bff2c7f7f2b0022d92eaa832b42a154584e98
Ojus Rane <ojusrane@LearniOSFreecoms-Mac.local>
2017-12-07 21:35:56 +0000

    Version 1.26
    - Finished commenting on TimerViewController.m
    - Need to start referencing

--- MultipleStopwatchTimer/TimerViewController.m
+++ MultipleStopwatchTimer/TimerViewController.m
@@ -51,7 +51,7 @@
     NSString *path = [[NSBundle mainBundle] pathForResource:@"apple_ring" ofType:@".mp3"]; //https://www.zedge.net/ringtone/1001866/
     NSURL *AlarmUrl = [NSURL fileURLWithPath:path];
     playAlarm = [[AVAudioPlayer alloc]initWithContentsOfURL:AlarmUrl error:NULL];
-        // Need to initialise the audio in the app. I had to do some research in how to add audio to the view controller. We need to insert a file which is under the "Supported Files" Folder. Before we set the audio, we need to declare an NSString and NSURL. The play alarm will use NSURL declared name and the NSURL will use the NSString variable name. In the pathForResource section, we must quote the audio file name that we are going to use and in the ofType section, we must quote the type of audio file we are using i.e. In this case it is .mp3 file. In the NSURL, we use a method that mentions were our audio file is located. In this case it is "path", as this is the location set. The playalarm variable will need to allocate the AVAudioplayer and its initWithContentsofURL will be the URL variable I declared which is "AlarmUrl". The error will be NULL (CARRY ON WITH THIS EXPLANATION)
+        // Need to initialise the audio in the app. I had to do some research in how to add audio to the view controller. We need to insert a file which is under the "Supported Files" Folder. Before we set the audio, we need to declare an NSString and NSURL. The play alarm will use NSURL declared name and the NSURL will use the NSString variable name. In the pathForResource section, we must quote the audio file name that we are going to use and in the ofType section, we must quote the type of audio file we are using i.e. In this case it is .mp3 file. In the NSURL, we use a method that mentions were our audio file is located. In this case it is "path", as this is the location set. The playalarm variable will need to allocate the AVAudioplayer and its initWithContentsofURL will be the URL variable I declared which is "AlarmUrl". The error is set to NULL, which means that it will ignore any errors present within the object
 }
 
 
@@ -69,7 +69,9 @@
     self.PickerViewSelector.hidden = true;
     self.TimerLabelOne.hidden = false;
     self.HoursMinutesSecondsLabelOne.hidden = true;
+        // When the timer is running, I want the picker view to be hidden and the Timer Label to be shown so we can keep track of the timer
     if (RunningTimerOne ==NO) {
+        // This Boolean expression will initially be NO and turn to YES when the timer is running
         RunningTimerOne = YES;
         self.ResetButtonOne.hidden = FALSE;
         [_StartButtonOne setTitle:@"Pause" forState:UIControlStateNormal];
@@ -79,12 +81,14 @@
         if (TimerOne == nil){
             TimerOne = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimerOne) userInfo:nil repeats:YES];
             [[NSRunLoop currentRunLoop] addTimer:TimerOne forMode:NSRunLoopCommonModes];
+            // The NSRunLoop method was very important in this app. It is there to improve the performance of the NSTimer and mainly needed so the timer is still running when you are scrolling the view
         }
     }
     else {
         [self PauseTimerOne];
     }
     [self FinishedTimerOneAlarm];
+        // This method will perform when the timer is up
 }
 }
 
@@ -115,7 +119,11 @@
         Seconds_TimerOne = 59;
     }
     self.TimerLabelOne.text = [NSString stringWithFormat: @"%02ld:%02ld:%02ld",Hours_TimerOne,Minutes_TimerOne,Seconds_TimerOne];
+    
+    // This method will perform when the NSTimer is running.
+    
     [self FinishedTimerOneAlarm];
+    // This method wil perform when the timer has reached 0 seconds
 }
 
 - (void) PauseTimerOne{
@@ -152,6 +160,8 @@
         self.TimerLabelOne.hidden = TRUE;
         [self InitialTimeSetOne];
         [self AlertNotification];
+        
+        //When the timer is up, all the neccesary functions will be performed including an Alert Notification which I have commented about on the bottom of this main file
     }
 }
 
@@ -407,7 +417,7 @@
     
     NSString *Number = [NSString stringWithFormat:@"%ld",row];
     return Number;
-    
+    //This delegate method was required so the picker view would display the row numbers in each component
 }
 
 - (void)pickerView:(UIPickerView *)pickerView
@@ -418,18 +428,20 @@
     _PickerViewSelectorTwo.tag = 2;
     _PickerViewSelectorThree.tag = 3;
     
+    //Before I give all 3 of my picker view some tag numbers, when I set the time for timer "1" and adjust the picker view for timer "2" or "3", timer "1" would reset back from the time it was originally set too. If you run my previous versions with all 3 timers, you will understand what I am talking about. Hence it was important for me to give each picker view a tag number so there would be no conflicts between the 3 timers
     
-    if (pickerView.tag ==1){
+    
+    if (pickerView.tag ==1){ //Timer one
         Hours_TimerOne = [self.PickerViewSelector selectedRowInComponent:0];
         Minutes_TimerOne = [self.PickerViewSelector selectedRowInComponent:1];
         Seconds_TimerOne = [self.PickerViewSelector selectedRowInComponent:2];
     }
-    else if (pickerView.tag ==2){
+    else if (pickerView.tag ==2){ //Timer Two
         Hours_TimerTwo = [self.PickerViewSelectorTwo selectedRowInComponent:0];
         Minutes_TimerTwo = [self.PickerViewSelectorTwo selectedRowInComponent:1];
         Seconds_TimerTwo = [self.PickerViewSelectorTwo selectedRowInComponent:2];
     }
-    else if (pickerView.tag ==3){
+    else if (pickerView.tag ==3){ //Timer Three
         Hours_TimerThree = [self.PickerViewSelectorThree selectedRowInComponent:0];
         Minutes_TimerThree = [self.PickerViewSelectorThree selectedRowInComponent:1];
         Seconds_TimerThree = [self.PickerViewSelectorThree selectedRowInComponent:2];
@@ -438,7 +450,7 @@
 
 #pragma mark Picker view Data Source Methods
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
-    return 3;
+    return 3; // This data source method was needed so it could display 3 components in the view
 }
 
 - (NSInteger)pickerView:(UIPickerView *)pickerView
@@ -446,18 +458,19 @@ numberOfRowsInComponent:(NSInteger)component{
     
     NSInteger rows;
     
-    if (component == 0) {
+    if (component == 0) { // 100 rows so the user can select a max hours of 99
         rows = 100;
     }
-    else if (component == 1) {
+    else if (component == 1) { // 60 rows so user can select up to 59 minutes
         rows = 60;
     }
-    else if (component == 2) {
+    else if (component == 2) { // 60 rows so user can select up to 59 seconds
         rows = 60;
     }
     
     return rows;
     
+    // This method was to set the number of rows in each component.
 }
 
 #pragma mark Alert Notification Method
@@ -466,12 +479,18 @@ numberOfRowsInComponent:(NSInteger)component{
     UIAlertAction *OKpressed = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){
         [alert dismissViewControllerAnimated:YES completion:nil]; [playAlarm stop];}];
     
-    if (alert != nil) {
+    // To set an alert notification, we need an Alert Controller that will display the alert message and an Alert Action that will give the user to perform an action when the Alert message has popped up. In this case we want the user to press OK to clear the Alert Message.
+    
+    // For UIAlertController, the alertControllerWithTitle is setting the title name, message is setting the text below the title, preferredStyle is setting the style of the alert controller.
+    
+    // For UIAlertAction, actionWithTitle is setting the text for the button, style is setting the style of the of the action and the handler is what you would like to happen when the OK button has been pressed. In this case we would like to dismiss the Alert Notification and stop the audio that we have set.
+    
+    if (alert != nil) { // i.e if alert is currently needed to be used, then perform the action in this if statement
         [playAlarm play];
         playAlarm.numberOfLoops = -1; //infinite loop
     }
-    [alert addAction:OKpressed];
-    [self presentViewController:alert animated:YES completion:nil];
+    [alert addAction:OKpressed]; // Adding an action to the alert
+    [self presentViewController:alert animated:YES completion:nil]; //Adding the Alert Notification to the view controller.
 }
 
 
